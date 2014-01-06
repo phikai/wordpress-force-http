2377559cd9885c190f83a56841b4729e176b8cfd
Kai Armstrong <kai.armstrong@gmail.com>
2014-01-06 09:46:59 -0600

    no more https... initial commit

---
+++ wordpress-force-http.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Plugin Name: WordPress Force HTTP
+ * Plugin URI: http://pressable.com
+ * Description: Forces the front end of your WordPress site to HTTP when you only want the Admin over HTTPS
+ * Author: A. Kai Armstrong <code@pressable.com>
+ * Author URI: http://www.kaiarmstrong.com
+ * Version: 0.1
+ * */
+
+#http://yoast.com/wordpress-ssl-setup/
+function pr_force_http () {
+  if ( is_ssl() && !is_admin() ) {
+    if ( 0 === strpos($_SERVER['REQUEST_URI'], 'http') ) {
+      wp_redirect(preg_replace('|^https://|', 'http://', $_SERVER['REQUEST_URI']), 301 );
+      exit();
+    } else {
+      wp_redirect('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
+      exit();
+    }
+  }
+}
+add_action ( 'template_redirect', 'pr_force_http', 1 );
